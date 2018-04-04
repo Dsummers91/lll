abfd81a939b1f4b405b3ae0277b4aa189171ca57
Deon <deonsummers91@gmail.com>
2018-04-03 19:33:03 -0500

    add owner method

--- crowdsale/Crowdsale.sol
+++ crowdsale/Crowdsale.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.4.15;
 
 contract Crowdsale {
+  address public owner;
   
   function startCrowdsale() {}
   function getAddress() constant returns (address) {}
   function getCrowdsaleState() public view returns (uint256) {}
+
 }
