11d935653c15b3cefa74009a09fd32794b93b8ce
Deon <deonsummers91@gmail.com>
2018-04-04 10:18:43 -0500

    added wei multiplier to constructor

--- crowdsale/Crowdsale.sol
+++ crowdsale/Crowdsale.sol
@@ -4,11 +4,12 @@ contract Crowdsale {
   address public owner;
   address public multisig;
   uint256 public maxSupply;
- 
-  function Crowdsale(address multisig, uint256 maxSupply) {}
+  uint256 public weiMultiplier;
+
+  function Crowdsale(address multisig, uint256 maxSupply, uint256 weiMultiplier) {}
  
   function startCrowdsale() {}
   function getAddress() constant returns (address) {}
   function getCrowdsaleState() public view returns (uint256) {}
-
+  function balanceOf(address) public view returns(uint256) {}
 }
